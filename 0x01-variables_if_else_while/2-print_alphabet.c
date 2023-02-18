546aac0b9d796f956804d9e3710049e42e565670
Bono20 <emirubiniyam@gmail.com>
2023-02-17 23:12:21 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowetcase.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		putchar(letter);
+	putchat('\n');
+
+	return (0);
+}
