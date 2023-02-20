9e5baa0b80de635170819c645d168fe6d3eb0eb9
Bono20 <emirubiniyam@gmail.com>
2023-02-20 04:57:20 -0800

    task 0 to 3

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Saize of a char: %lu byte(s)\n, (unsigned long)sizeof(d));
+	printf("Saize of an int : % lu byte(s)\n, (unsigned long)sizeof(a));
+	printf("Saize of a long int: %lu byte(s)\n, (unsigned long)sizeof(b));
+	printf("Saize of a long long int : % lu byte(s)\n, (unsigned long)sizeof(c));
+	printf("Saize of a float: %lu byte(s)\n, (unsigned long)sizeof(f));
+	return (0);
+
