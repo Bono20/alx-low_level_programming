c1de58f09094c9b5ecdec9d857f2173f3b4b5041
Bono20 <emirubiniyam@gmail.com>
2023-03-06 08:24:24 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,8 @@
  * @haystack: the longer string to search
  * @needle: the substring to search for
  *
- * Return: a pointer to the beginning of the located substring, or NULL if
+ * Return: a pointer to the beginning
+ * of the located substring, or NULL if
  * the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
