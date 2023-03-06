170e8d81ab4028b9a008fd3105098b2a99e50924
Bono20 <emirubiniyam@gmail.com>
2023-03-06 08:16:41 -0800

    task 2 to 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 /**
  * _strstr - locates a substring
- * haystack: the longer string to search
+ *
+ * @haystack: the longer string to search
  * @needle: the substring to search for
+ *
  * Return: a pointer to the beginning of the located substring, or NULL if
  * the substring is not found.
  */
