52e8701a2e96a2f15d0cf3fb841f58d369755b33
Bono20 <emirubiniyam@gmail.com>
2023-03-14 13:49:16 -0700

    tasks

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,8 +7,9 @@ char *_strdup(char *str);
 int _strlen(char *s);
 char *str_concat(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
-int **alloc_grid(int width, int height);
+int **malloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
