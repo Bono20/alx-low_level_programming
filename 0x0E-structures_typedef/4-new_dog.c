f408573006754d7e134519c15935d86e21c4a663
Bono20 <emirubiniyam@gmail.com>
2023-03-20 12:31:46 -0700

    task wow

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+/**
+ * new_dog - ...
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ *
+ * Return: ...
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *cutie_dog;
+	int name_l = 0, won_l = 0;
+
+	if (name != NULL && owner != NULL)
+	{
+		2222
